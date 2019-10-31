3631d0c49d8602e926de3cbf40e81e476f854dba
Adam Lee <adam.lee@students.plymouth.ac.uk>
2019-10-31 10:20:13 +0000

    Added login input fields and buttons

--- src/app/Views/login/login.module.ts
+++ src/app/Views/login/login.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login.page';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ LoginComponent ],
   imports: [
+    IonicModule,
+    FormsModule,
     CommonModule,
     RouterModule.forChild([{ path: '', component: LoginComponent }])
   ]
--- src/app/Views/login/login.page.ts
+++ src/app/Views/login/login.page.ts
@@ -7,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: String;
+  password: String;
+
   constructor() { }
 
+  loginPressed(): void{
+    console.log(this.email, this.password)
+  }
+
   ngOnInit() {}
+
+ 
 
 }
