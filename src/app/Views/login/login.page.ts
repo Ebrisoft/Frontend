d7de935f31f650b740cc46883468497cd36c5f2b
Adam Lee <adam.lee@students.plymouth.ac.uk>
2019-10-31 10:50:23 +0000

    Added input validation for email and password

--- src/app/Views/login/login.page.ts
+++ src/app/Views/login/login.page.ts
@@ -7,17 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  
   email: String;
   password: String;
+  loginError:Boolean;
 
   constructor() { }
 
-  loginPressed(): void{
-    console.log(this.email, this.password)
+  LoginPressed(): void{
+   // this.loginError = false;
+    this.loginError = this.Validate()
+  }
+  
+  Validate(): Boolean {
+    if (this.email && this.password){
+      console.log(this.email.match(this.EMAIL_REGEX))
+      if (this.email.search(this.EMAIL_REGEX) != -1 ){
+        return false
+      }
+    }
+    return true;
   }
 
   ngOnInit() {}
-
- 
 
 }
