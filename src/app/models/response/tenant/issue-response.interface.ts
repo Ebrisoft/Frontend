0e696045b3dd2da14ecd52d88a1986df4976ca7e
Adam Lee <adam.lee@students.plymouth.ac.uk>
2019-11-27 13:41:05 +0000

    Fix title and author and title typo

--- src/app/models/response/tenant/issue-response.interface.ts
+++ src/app/models/response/tenant/issue-response.interface.ts
@@ -1,9 +1,9 @@
 import IUserResponse from "../common/user-response.interface";
 
 export default interface IIssueResponse {
-  tile: string;
+  title: string;
   content: string;
   createdAt: string;
   isResloved: boolean;
-  aurthor: IUserResponse;
+  author: IUserResponse;
 }
