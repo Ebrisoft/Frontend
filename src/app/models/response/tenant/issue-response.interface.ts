a9392abe6771fae34df8dcadcc3c8fd3a356365f
Adam Lee <adam.lee@students.plymouth.ac.uk>
2019-11-27 13:32:19 +0000

    Issue is now buffed out with all issue content

--- src/app/models/response/tenant/issue-response.interface.ts
+++ src/app/models/response/tenant/issue-response.interface.ts
@@ -1,3 +1,9 @@
+import IUserResponse from "../common/user-response.interface";
+
 export default interface IIssueResponse {
+  tile: string;
   content: string;
+  createdAt: string;
+  isResloved: boolean;
+  aurthor: IUserResponse;
 }
