27811e3fdd11d6c825ffbca9c5cd7dceef40dcc2
Toby Bessant <toby.bessant.1998@gmail.com>
2019-11-27 13:44:34 +0000

    Added additional fields and models required
    Date parsed

--- src/app/components/landlord/issue-card/issue-card.component.ts
+++ src/app/components/landlord/issue-card/issue-card.component.ts
@@ -9,10 +9,11 @@ import IIssueResponse from "src/app/models/response/landlord/issue-response.inte
 export class IssueCardComponent implements OnInit {
 
   @Input() issue: IIssueResponse;
+  private parsedDate: string;
 
-  constructor() { 
+  constructor() {}
+
+  ngOnInit() {
+    this.parsedDate = new Date(this.issue.createdAt).toLocaleDateString();
   }
-
-  ngOnInit() {}
-
 }
--- src/app/models/response/landlord/issue-response.interface.ts
+++ src/app/models/response/landlord/issue-response.interface.ts
@@ -1,6 +1,11 @@
 import IHouseResponse from "./house-response.interface";
+import IUserResponse from "../common/user-response.interface";
 
 export default interface IIssueResponse {
+  title: string;
   content: string;
+  createdAt: string;
+  isResolved: boolean;
+  author: IUserResponse;
   house: IHouseResponse;
 }
