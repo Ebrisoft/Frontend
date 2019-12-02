baf280b62cd4562afa7493806ce0041d5ad96895
Toby Smith <tobysmith568@hotmail.co.uk>
2019-12-02 16:21:26 +0000

    Implemented the Angular httpClient into the Cordova http service

--- src/app/services/http/http-cordova.ts
+++ src/app/services/http/http-cordova.ts
@@ -1,43 +1,28 @@
 import HTTPAbstract from "./http-service";
-import { HTTP, HTTPResponse } from "@ionic-native/http/ngx";
 import IHTTPResponse from "./http-response.interface";
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
 })
 export default class HTTPCordova implements HTTPAbstract {
-  private static http: HTTP = new HTTP();
 
-  constructor() {
-    
+  constructor(private http: HttpClient) {
   }
 
   async post(url: string, body: object, headers: any): Promise<IHTTPResponse> {
-    
-    // const rrresult = await HTTPCordova.http.post(url, body, headers);
 
-    const result: IHTTPResponse = {
-      body: "",
-      headers: "",
-      statusCode: -1
-    };
-
-    await HTTPCordova.http.sendRequest(url, {
-      method: "post",
-      serializer: "json",
+    const result = await this.http.post(url, body, {
       headers: headers,
-      data: body
-    }).then((res) => {
-      result.body = res.data,
-      result.headers = res.headers,
-      result.statusCode = res.status;
-    }).catch((res) => {
-      result.body = res.error,
-      result.headers = res.headers,
-      result.statusCode = res.status;
-    });
+      withCredentials: true,
+      observe: "response"
+    }).toPromise();
 
-    return result;
+    return {
+      body: result.body,
+      headers: result.headers,
+      statusCode: result.status
+    } as IHTTPResponse;
   }
 }
