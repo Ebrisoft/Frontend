4c3b9251dbff9aa5bbdf5e2e880dd786640a8a77
Adam Lee <adam.lee@students.plymouth.ac.uk>
2019-12-02 17:22:07 +0000

    Populate list of houses on SelectHouse init

--- src/app/components/landlord/house-list-item/house-list-item.component.ts
+++ src/app/components/landlord/house-list-item/house-list-item.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import IHouseResponse from "src/app/models/response/landlord/house-response.interface";
 
 @Component({
-  selector: 'app-house-list-item',
-  templateUrl: './house-list-item.component.html',
-  styleUrls: ['./house-list-item.component.scss'],
+  selector: "app-house-list-item",
+  templateUrl: "./house-list-item.component.html",
+  styleUrls: ["./house-list-item.component.scss"],
 })
 export class HouseListItemComponent implements OnInit {
 
+  @Input() house: IHouseResponse;
   constructor() { }
 
   ngOnInit() {}
--- src/app/views/landlord/house-select/house-select.module.ts
+++ src/app/views/landlord/house-select/house-select.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { IonicModule } from "@ionic/angular";
 import { HouseSelectPage } from "./house-select.page";
 import LandlordHouseAPIService from "src/app/services/api/landlord/houses-api.service";
+import { HouseListItemComponent } from "src/app/components/landlord/house-list-item/house-list-item.component";
 
 @NgModule({
   imports: [
@@ -14,6 +15,8 @@ import LandlordHouseAPIService from "src/app/services/api/landlord/houses-api.se
     RouterModule.forChild([{path: "", component: HouseSelectPage}])
   ],
   providers: [ LandlordHouseAPIService ],
-  declarations: [HouseSelectPage]
+  declarations: [
+    HouseSelectPage,
+    HouseListItemComponent]
 })
 export class HouseSelectPageModule {}
