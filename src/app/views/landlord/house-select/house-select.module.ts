c9926113d0f1925f991f72c310afdfe0a4b8f4b5
Adam Lee <adam.lee@students.plymouth.ac.uk>
2019-12-02 17:11:48 +0000

    Added house service and makes request on house selector view

--- src/app/views/landlord/house-select/house-select.module.ts
+++ src/app/views/landlord/house-select/house-select.module.ts
@@ -1,26 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
-import { IonicModule } from '@ionic/angular';
-
-import { HouseSelectPage } from './house-select.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: HouseSelectPage
-  }
-];
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
+import { IonicModule } from "@ionic/angular";
+import { HouseSelectPage } from "./house-select.page";
+import LandlordHouseAPIService from "src/app/services/api/landlord/houses-api.service";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild([{path: "", component: HouseSelectPage}])
   ],
+  providers: [ LandlordHouseAPIService ],
   declarations: [HouseSelectPage]
 })
 export class HouseSelectPageModule {}
--- src/app/views/landlord/house-select/house-select.page.ts
+++ src/app/views/landlord/house-select/house-select.page.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Inject } from "@angular/core";
+import { Router } from "@angular/router";
 import { Location } from "@angular/common";
+import LandlordHouseAPIService from "src/app/services/api/landlord/houses-api.service";
+import IHouseResponse from "src/app/models/response/landlord/house-response.interface";
 
 @Component({
-  selector: 'app-house-select',
-  templateUrl: './house-select.page.html',
-  styleUrls: ['./house-select.page.scss'],
+  selector: "app-house-select",
+  templateUrl: "./house-select.page.html",
+  styleUrls: ["./house-select.page.scss"],
 })
 export class HouseSelectPage implements OnInit {
 
-  constructor(private router: Router, private location: Location) { }
+  houses: IHouseResponse[];
 
-  ngOnInit() {
+  constructor(@Inject(LandlordHouseAPIService) private feedAPIService: LandlordHouseAPIService, private router: Router, private location: Location) { }
+
+  async ngOnInit() {
+    const response = await this.feedAPIService.getHouses();
+    this.houses = response.payload;
+    console.log(this.houses);
   }
 
   routeBack() {
