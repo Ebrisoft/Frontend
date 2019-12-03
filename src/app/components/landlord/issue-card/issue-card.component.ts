92fbd40dc691c50ba4706d72f48bad634537d2ae
Toby Bessant <toby.bessant.1998@gmail.com>
2019-12-03 10:21:37 +0000

    Updated avatar icon to be name initials. Secondary colour is pulled from ion-color-primary

--- src/app/components/landlord/issue-card/issue-card.component.ts
+++ src/app/components/landlord/issue-card/issue-card.component.ts
@@ -12,11 +12,13 @@ export class IssueCardComponent implements OnInit {
   @Input() issue: IIssueResponse;
   private parsedDate: string;
   private priorityDisplay: string; 
+  private secondaryColour: string;
 
   constructor() {}
 
   ngOnInit() {
     this.parsedDate = new Date(this.issue.createdAt).toLocaleDateString();
     this.priorityDisplay = Priority[this.issue.priority];
+    this.secondaryColour = getComputedStyle(document.body).getPropertyValue("--ion-color-secondary").trim().substring(1);
   }
 }
