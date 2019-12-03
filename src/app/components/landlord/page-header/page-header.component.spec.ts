2ee34e4849655b04f85f1a87e12496e9d4987133
Toby Smith <tobysmith568@hotmail.co.uk>
2019-12-03 14:05:06 +0000

    Update page-header.component.spec.ts

--- src/app/components/landlord/page-header/page-header.component.spec.ts
+++ src/app/components/landlord/page-header/page-header.component.spec.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-
 import { PageHeaderComponent } from "./page-header.component";
 
 describe("PageHeaderComponent", () => {
