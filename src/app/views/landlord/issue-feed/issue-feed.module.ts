3507cddba640610c85186eca0aceeae9b6c5f10f
Toby Smith <tobysmith568@hotmail.co.uk>
2019-12-03 15:41:12 +0000

    Update issue-feed.module.ts

--- src/app/views/landlord/issue-feed/issue-feed.module.ts
+++ src/app/views/landlord/issue-feed/issue-feed.module.ts
@@ -7,7 +7,7 @@ import LandlordFeedAPIService from "src/app/services/api/landlord/feed-api-servi
 import { IssueCardComponent } from "src/app/components/landlord/issue-card/issue-card.component";
 import { PageHeaderModule } from "src/app/components/landlord/page-header/page-header.module";
 import { CurrentHouseService } from "src/app/services/observables/current-house-service";
-import { FilterIssuesPipe } from 'src/app/services/pipes/filter-issues.pipe';
+import { FilterIssuesPipe } from "src/app/services/pipes/filter-issues.pipe";
 
 @NgModule({
   imports: [
