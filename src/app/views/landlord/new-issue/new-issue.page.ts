fe188bda07a166f201ba04aa1c8f2933c5bf7321
Toby Bessant <toby.bessant.1998@gmail.com>
2019-12-11 02:34:37 +0000

    Added house priority input

--- src/app/views/landlord/new-issue/new-issue.page.ts
+++ src/app/views/landlord/new-issue/new-issue.page.ts
@@ -5,6 +5,9 @@ import { Location } from "@angular/common";
 import { ToastController, PickerController } from "@ionic/angular";
 import { PickerOptions } from "@ionic/core";
 import { Priority } from "../../../utils/priority.enum";
+import LandlordHouseAPIService from "src/app/services/api/landlord/houses-api.service";
+import IHouseResponse from "src/app/models/response/landlord/house-response.interface";
+import { CurrentHouseService } from "src/app/services/observables/current-house-service";
 
 @Component({
   selector: "app-new-issue",
@@ -12,6 +15,8 @@ import { Priority } from "../../../utils/priority.enum";
   styleUrls: ["./new-issue.page.scss"],
 })
 export class NewIssuePage implements OnInit {
+
+  private houses: IHouseResponse[];
 
   title: string;
   content: string;
@@ -22,8 +27,13 @@ export class NewIssuePage implements OnInit {
 
   isFormValid: boolean;
 
-  constructor(private landlordIssueAPIService: LandlordIssueAPIService, public toastController: ToastController, 
-    private router: Router, private location: Location, private pickerCtrl: PickerController) {
+  constructor(private landlordIssueAPIService: LandlordIssueAPIService,
+    private houseApiService: LandlordHouseAPIService,
+    private currentHouseService: CurrentHouseService,
+    public toastController: ToastController, 
+    private router: Router, 
+    private location: Location, 
+    private pickerCtrl: PickerController) {
       this.isFormValid = false;
   }
 
@@ -34,14 +44,16 @@ export class NewIssuePage implements OnInit {
           text: "Done"
         }
       ],
-      columns: [{
-        name: "Priority",
-        options: [
-          {text: "Low", value: "0"},
-          {text: "Medium", value: "1"},
-          {text: "High", value: "2"},
-        ]
-      }]
+      columns: [
+        {
+          name: "Priority",
+          options: [
+            { text: "Low", value: "0" },
+            { text: "Medium", value: "1" },
+            { text: "High", value: "2" }
+          ]
+        }
+      ]
     };
 
     const picker = await this.pickerCtrl.create(opts);
@@ -64,7 +76,7 @@ export class NewIssuePage implements OnInit {
 
   async checkPressed(): Promise<void> {
     if (this.isFormValid) {
-      const response = await this.landlordIssueAPIService.createIssue(this.title, this.content, this.houseId, this.priority);
+      await this.landlordIssueAPIService.createIssue(this.title, this.content, this.houseId, this.priority);
       this.routeBack();
     } else {
       this.presentToast();
@@ -77,23 +89,21 @@ export class NewIssuePage implements OnInit {
 
   checkChange() {
     this.isFormValid = true;
-
-    if (this.title.length === 0) {
+    if (!this.title || !this.content || !this.houseId || !this.priority) {
       this.isFormValid = false;
     }
-
-    if (this.content.length === 0) {
-      this.isFormValid = false;
-    }
-
-    console.log("Valid: " + this.isFormValid);
   }
 
-  ngOnInit() {
-    this.title = this.content = "";
-    this.houseId = 1;
-    this.priority = 0;
+  async ngOnInit() {
+    const fetchHouses = await this.houseApiService.getHouses();
+    this.houses = fetchHouses.payload;
 
+    this.title = this.content = "";
+    this.priority = 1;
+    if (this.currentHouseService.getCurrentHouse()) {
+      this.houseId = this.currentHouseService.getCurrentHouse().id;
+    }
+    
     this.priorityString = Priority[this.priority];
   }
 }
