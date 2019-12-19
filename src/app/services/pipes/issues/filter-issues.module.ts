4dc0779f36c7c89aa351d908bb82c16f6233c268
Adam Lee <adam.lee@students.plymouth.ac.uk>
2019-12-19 13:23:54 +0000

    Tenant issues now ordered by creation time

--- src/app/services/pipes/issues/filter-issues.module.ts
+++ src/app/services/pipes/issues/filter-issues.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { FilterIssuesByHouse, FilterIssuesByStatus } from "./filter-issues.pipe";
+import { OrderByDate } from "./order-by-date.pipe";
 
 @NgModule({
   imports: [],
-  declarations: [ FilterIssuesByHouse, FilterIssuesByStatus ],
-  exports: [ FilterIssuesByHouse, FilterIssuesByStatus ],
+  declarations: [ FilterIssuesByHouse, FilterIssuesByStatus, OrderByDate ],
+  exports: [ FilterIssuesByHouse, FilterIssuesByStatus, OrderByDate ],
 })
 export class IssueFilters { }
